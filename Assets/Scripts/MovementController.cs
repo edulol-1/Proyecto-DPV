f3ab1aa02cb45bdc5e5f9671793536f88ee0d889
edulol-1 <edu3000@ciencias.unam.mx>
2025-04-18 23:54:22 -0600

    Camera position and unused namespaces

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO.Compression;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class MovementController : MonoBehaviour
