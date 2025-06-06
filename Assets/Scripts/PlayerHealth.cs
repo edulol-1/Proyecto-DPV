a038b55ac2fc4acc3c7c71df70ff3f255572e320
edulol-1 <edu3000@ciencias.unam.mx>
2025-06-05 22:14:16 -0600

    Add death and respawn logic

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
+
 
 public class PlayerHealth : MonoBehaviour
 {
     [Header("Health Settings")]
     public int maxHealth = 100;
     public int currentHealth;
-    
+
     [Header("UI Settings")]
     public Slider healthSlider;
     public TextMeshProUGUI healthText; // Cambiado de Text a TextMeshProUGUI
+
+    [Header("Respawn Settings")]
+    public Transform[] spawnPoints;
+    public float respawnDelay = 2f;
+    public int deaths = 0;
+    //public Transform playerTrans;
+
 
     private void Start()
     {
@@ -25,7 +34,7 @@ public class PlayerHealth : MonoBehaviour
             healthSlider.maxValue = maxHealth;
             healthSlider.value = currentHealth;
         }
-        
+
         if (healthText != null)
         {
             healthText.text = $"Health: {currentHealth}/{maxHealth}";
@@ -37,7 +46,7 @@ public class PlayerHealth : MonoBehaviour
         currentHealth -= damageAmount;
         currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
         UpdateHealthUI();
-        
+
         if (currentHealth <= 0)
         {
             Die();
@@ -47,5 +56,39 @@ public class PlayerHealth : MonoBehaviour
     private void Die()
     {
         Debug.Log("Player is Dead");
+        gameObject.SetActive(false);
+        Invoke(nameof(Respawn), respawnDelay);
+        if (++deaths == 3)
+        {
+            Invoke(nameof(ShowResults), 2);      
+        }
+    }
+
+    private void Respawn()
+    {
+        if (spawnPoints.Length > 0)
+        {
+            int index = Random.Range(0, spawnPoints.Length);
+            //playerTrans.position = spawnPoints[index].position;
+            transform.position = spawnPoints[index].position;
+        }
+
+        currentHealth = maxHealth;
+        UpdateHealthUI();
+        gameObject.SetActive(true);
+    }
+
+    private void ShowResults()
+    {
+        string[] results = (gameObject.tag == "Player2")
+        ? new string[] { "Player 1", "Player 2" }
+        : new string[] { "Player 2", "Player 1" };
+
+        Debug.Log(results[0] + " won the game! :)");
+        Debug.Log(results[1] + " lost the game :(");
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Time.timeScale = 0f;
+        SceneManager.LoadScene("Title");
     }
 }
